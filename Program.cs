a6935fa2b052230bf9eba17a341e440d92833f9b
Ross S <developer@iname.com>
2023-07-21 14:27:08 +0100

    Sliding Window Leetcode#3

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Solution firstSolution = new Solution();
+        string insertString = "abcabcbb";
+        int returnedStringValue = firstSolution.slidingWindowMethodLongestSubstring(insertString);
+        Console.WriteLine(returnedStringValue);
+    }
+}
+class Solution
+{
+    public Solution(){}
+    public int slidingWindowMethodLongestSubstring(string s)
+    {
+        char[] charArrayFromString = s.ToCharArray();
+        int right = 0, maxSubstring = 0;
+        bool repeatedCharacter = false;
+        System.Text.StringBuilder solutionStringBuilder = new System.Text.StringBuilder();
+        for(right = 0; right < charArrayFromString.Length; right++)
+        {
+            repeatedCharacter = solutionStringBuilder.ToString().Contains(charArrayFromString[right]);
+            if (repeatedCharacter)
+            {
+                solutionStringBuilder.Remove(0, solutionStringBuilder.ToString().IndexOf(charArrayFromString[right]) + 1);
+            }
+            solutionStringBuilder.Append(charArrayFromString[right]);
+            maxSubstring = (maxSubstring < solutionStringBuilder.Length) ? maxSubstring = solutionStringBuilder.Length : maxSubstring;
+        }
+        return maxSubstring;
+    }   
+}
+
