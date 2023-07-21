516b13f58235042c654106abd91c87693c047257
Dev - Main Pc <developer@iname.com>
2023-07-21 22:32:44 +0100

    Simplifying Code

--- Program.cs
+++ Program.cs
@@ -14,20 +14,18 @@ class Solution
     public int slidingWindowMethodLongestSubstring(string s)
     {
         char[] charArrayFromString = s.ToCharArray();
-        int right = 0, maxSubstring = 0;
-        bool repeatedCharacter = false;
+        int maxSubstring = 0, indexOfChar = -1;
         System.Text.StringBuilder solutionStringBuilder = new System.Text.StringBuilder();
-        for(right = 0; right < charArrayFromString.Length; right++)
+        for(int right = 0; right < charArrayFromString.Length; right++)
         {
-            repeatedCharacter = solutionStringBuilder.ToString().Contains(charArrayFromString[right]);
-            if (repeatedCharacter)
+            indexOfChar = solutionStringBuilder.ToString().IndexOf(charArrayFromString[right]);
+            if (indexOfChar != -1)
             {
-                solutionStringBuilder.Remove(0, solutionStringBuilder.ToString().IndexOf(charArrayFromString[right]) + 1);
+                solutionStringBuilder.Remove(0, indexOfChar + 1);
             }
             solutionStringBuilder.Append(charArrayFromString[right]);
-            maxSubstring = (maxSubstring < solutionStringBuilder.Length) ? maxSubstring = solutionStringBuilder.Length : maxSubstring;
+            Math.Max(maxSubstring, solutionStringBuilder.Length);
         }
         return maxSubstring;
     }   
 }
-
